12a7ceefe10ea0df553aa4d7127a2341f5bd1295
Alfred-Acquah <alfredacquah20@gmail.com>
2022-10-14 15:12:10 -0700

    suffer

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,14 +3,14 @@
 /**
  * main - prints the alphabets in lowercase,
  * followed by a new line
- * Return: Always 0 (Success)*
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{	
+	{
 		putchar(ch);
 	}
 	putchar('\n');
